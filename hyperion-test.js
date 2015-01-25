dbe651a4a2ad2db42e86e8125e4274df2c78ef69
Krzysztof Kabat <odrinwhite@hotmail.com>
2015-01-25 12:14:56 +0000

    first alpha

--- hyperion-test.js
+++ hyperion-test.js
@@ -4,35 +4,95 @@ var port = process.env.PORT || 8080;
 var server = new hyperion(port,'/public/')
 var playerId = 0
 var tmp = 0
+
+
+function findPlayer(players, id){
+    var player = null
+    players.forEach(function(el){
+        if(el.id === id && el.connected){
+            player = el
+        }
+    })
+    return player
+}
+
 var board = {
     data :{
+        counter : 15,
+        currentIll : '',
+        sickId : 0,
         players : []
     },
     text : 'test',
     join : function(ws, name){
         var found = false
         board.data.players.forEach(function(el){
-            if(el.id === ws.data.id){
+            if(el.id === ws.data.id && el.connected){
                 el.name = name
                 found = true
             }
         })
         if(!found){ 
             board.data.players.push({
+                x : Math.floor((Math.random() * 800) - 400),
+                y : Math.floor((Math.random() * 800) - 400),
+                score : 0,
                 name : name,
-                id : ws.data.id
+                id : ws.data.id,
+                connected : true
             })
         }
         board.text = 'id: ' + String(tmp++)
         return 'sucessfully joined'
+    },
+    move : function(ws,x,y){
+        var player = findPlayer(board.data.players,ws.data.id)
+        if(player !== null){
+            var dx = x - player.x
+            var dy = y - player.y
+            
+            var length = Math.sqrt(dx*dx + dy*dy)
+            
+            var ndx = dx / length
+            var ndy = dy / length
+            
+            player.x = player.x +(ndx*100)
+            player.y = player.y +(ndy*100)
+        }
     }
 }
+
+setInterval(function(){
+    board.data.counter--
+    if(board.data.counter === 0){
+        board.data.counter = 15
+        if(board.data.players.length > 0){
+            while(true){
+                var num = Math.floor((Math.random() * board.data.players.length))
+                if(board.data.players[num].connected){
+                    board.data.currentIll = board.data.players[num].name
+                    board.data.sickId = board.data.players[num].id
+                    break
+                }
+            }
+        }
+    }
+},1000)
 
 server.registerNewConnection(function(ws){
     ws.data = {
         id : playerId++
     }
     console.log('new connection registered');
+})
+
+server.registerOnDisconnect(function(ws){
+    board.data.players.forEach(function(el){
+        if(el.id === ws.data.id){
+            el.connected = false
+        }
+    })
+    console.log('connection unregistered');
 })
 
 server.registerObject('board',board)
--- public/js/game-controller.js
+++ public/js/game-controller.js
@@ -2,59 +2,191 @@ var phonecatApp = angular.module('gameApp', []);
 
 
 
-phonecatApp.controller('GameController', function ($scope) {
-    
-    function noCall(){console.warn('call not reached')}
-    
-    function onMessage(apply){
+phonecatApp.controller('GameController', function($scope) {
+
+    function noCall() {
+        console.warn('call not reached')
+    }
+
+    function onMessage(apply) {
         console.log('onMessage')
-        $scope.$apply(function () {
+        $scope.$apply(function() {
             apply()
             console.log('scope applied')
         })
     }
-    
+
     function onOpen() {
         console.log('onOpen')
-        client.getObject('board').then(function(board){
-            
+        client.getObject('board').then(function(board) {
+
             console.log('received board')
             $scope.board = board
-            /*object.doSth().then(function(result){
-                console.log(result)
-            })*/
-            /*Object.observe(deep(object), function(changes){
-                console.log(changes)
-            })*/
-            
-            client.getObject('player').then(function(player){
-                player.move('up').then(function(result){
-                    console.log('result '+result)
+                /*object.doSth().then(function(result){
+                    console.log(result)
+                })*/
+                /*Object.observe(deep(object), function(changes){
+                    console.log(changes)
+                })*/
+
+            client.getObject('player').then(function(player) {
+                player.move('up').then(function(result) {
+                    console.log('result ' + result)
                 })
             })
         })
     }
 
     var client = Nautilus.createClient({
-        host : 'wss://hyperion-test-odrinwhite1.c9.io',
-        onopen : onOpen,
-        onmessage : onMessage
+        host: 'wss://hyperion-test-odrinwhite1.c9.io',
+        onopen: onOpen,
+        onmessage: onMessage
     })
-    
+
     $scope.board = {
-        data : {
-            players : []
+        data: {
+            players: []
         },
-        join : noCall
+        join: noCall
     }
+    $scope.joined = false
     $scope.playername = getCookie('playername')
-    $scope.joinGame = function(playername){
-        if(getCookie('playername') !== ''){
+    $scope.joinGame = function(playername) {
+        if (getCookie('playername') !== '') {
             eraseCookie('playername')
         }
-        createCookie('playername',playername,10)
+        createCookie('playername', playername, 10)
         $scope.playername = playername
+        $scope.board.join(playername).then(function() {
+            $scope.joined = true
+            initGame()
+        })
     }
+
+    function initGame() {
+        var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'game-canvas', {
+            preload: preload,
+            create: create,
+            update: update,
+            render: render
+        });
+        
+        var sprite
+
+        function preload() {
+
+            game.stage.backgroundColor = '#007236';
+
+            //game.load.image('ball', 'assets/sprites/shinyball.png');
+            game.load.spritesheet('mushroom', 'assets/grzybsheet.png',64,64);
+            //game.load.image('phaser', 'assets/sprites/sonic_havok_sanity.png');
+
+        }
+
+        var cursors;
     
-    
+        var sprite = null
+        
+        var mouseDown = false
+        
+        var timer = null
+        
+        var players = []
+        
+        var style = { font: "24px Arial", fill: "#00ff00", align: "center" };
+
+        function create() {
+
+            //  Modify the world and camera bounds
+            game.world.setBounds(-1000, -1000, 2000, 2000);
+
+            
+
+            //  This will create a new object called "cursors", inside it will contain 4 objects: up, down, left and right.
+            //  These are all Phaser.Key objects, so anything you can do with a Key object you can do with these.
+            cursors = game.input.keyboard.createCursorKeys();
+            game.input.onDown.add(function(){
+                mouseDown = true
+            })
+            
+            game.input.onUp.add(function(){
+                mouseDown = false
+            })
+            
+            //  Create our Timer
+            timer = game.time.create(false);
+            timer.loop(300, updateCounter, this);
+            timer.start();
+        }
+        
+        function updateCounter(){
+            if(mouseDown){
+                $scope.board.move(game.input.x - 32, game.input.y - 32)
+            }
+            var playerId = findPlayer($scope.board.data.players, $scope.board.data.sickId)
+            
+            if(playerId !== -1){
+                players.forEach(function(p){
+                    p.mySprite.frame = 0
+                })
+                players[playerId].mySprite.frame = 1
+            }
+        }
+        
+        function createPlayerSprite(player){
+            console.log('sprite created')
+            var playerGroup = game.add.group();
+            playerGroup.x  = player.x
+            playerGroup.y = player.y
+            var sprite = game.add.image(-32, -32, 'mushroom');
+            playerGroup.add(sprite)
+            sprite.frame = 0
+            var t = game.add.text(-32, -64, player.name, style)
+            playerGroup.add(t)
+            playerGroup.mySprite = sprite
+            return playerGroup
+        }
+        
+        function findPlayer(players, id){
+            var result = -1;
+            players.forEach(function(el){
+                if(el.id === id && el.connected){
+                    result = el.id
+                }
+            })
+            return result
+        }
+
+        function update() {
+            
+            if(players.length != $scope.board.data.players.length){
+                
+                for(var i = players.length ; i < $scope.board.data.players.length; ++i){
+                    players[i] = createPlayerSprite($scope.board.data.players[i])
+                }
+            }
+            
+            for(var i = 0; i < $scope.board.data.players.length; ++i){
+                if(players[i].x !== $scope.board.data.players[i].x || players[i].y !== $scope.board.data.players[i].y){
+                    var tween = game.add.tween(players[i]);
+                    tween.to({ 
+                        x: $scope.board.data.players[i].x + 32,
+                        y: $scope.board.data.players[i].y}, 290);
+                    tween.start();
+                }
+            }
+            
+            
+        }
+
+        function render() {
+
+            game.debug.cameraInfo(game.camera, 32, 500);
+
+        }
+    }
+
+
+
+
 });
